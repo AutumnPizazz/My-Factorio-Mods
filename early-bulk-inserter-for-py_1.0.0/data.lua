43b1721bf422fb2a88d8b2cda600b9830b020481
AutumnPizazz <hurxwork@outlook.com>
2025-03-08 12:46:01 +0800

    试图修复不生效的错误,失败

--- early-bulk-inserter-for-py_1.0.0/data.lua
+++ early-bulk-inserter-for-py_1.0.0/data.lua
@@ -6,14 +6,14 @@ bulk_inserter_recipe.ingredients = {
     {"small-parts-01", 2},
     {"iron-plate", 5}
 }
-bulk_inserter_recipe.result.count = 1  -- 每次制作获得1个集装机械臂
+-- bulk_inserter_recipe.result.count = 1  -- 每次制作获得1个集装机械臂
 
 -- 获取相关的科技
 local bulk_inserter_tech = data.raw["technology"]["bulk-inserter"]
 
 -- 修改科技的效果和研究条件
 bulk_inserter_tech.effects = {
-    {type = "unlock-recipe", recipe = "bulk-inserte"}
+    {type = "unlock-recipe", recipe = "bulk-inserter"}
 }
 bulk_inserter_tech.prerequisites = {"engine"}
 bulk_inserter_tech.unit = {
