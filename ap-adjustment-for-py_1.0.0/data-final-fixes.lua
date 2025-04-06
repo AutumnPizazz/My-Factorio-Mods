f4a8a851d6e2eb77124cbf2563fa1aee1242511e
AutumnPizazz <hurxwork@outlook.com>
2025-04-06 14:53:28 +0800

    update

---
+++ ap-adjustment-for-py_1.0.0/data-final-fixes.lua
@@ -0,0 +1,41 @@
+data.raw.recipe["mdrn-loader"].ingredients = {
+    {
+        type = "item",
+        name = "small-parts-01",
+        amount = 2
+    },
+    {
+        type = "item",
+        name = "iron-plate",
+        amount = 5
+    }
+}
+
+data.raw.recipe["splitter"].ingredients = {
+    {
+        type = "item",
+        name = "small-parts-01",
+        amount = 2
+    },
+    {
+        type = "item",
+        name = "iron-plate",
+        amount = 5
+    }
+}
+
+data.raw.recipe["programmable-speaker"].ingredients = {
+    {
+        type = "item",
+        name = "small-parts-01",
+        amount = 1
+    }
+}
+
+data.raw.recipe["display-panel"].ingredients = {
+    {
+        type = "item",
+        name = "small-parts-01",
+        amount = 1
+    }
+}
