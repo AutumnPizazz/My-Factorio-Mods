85491336e08c420d5179d37d919f3b89e8eba2f8
AutumnPizazz <hurxwork@outlook.com>
2025-04-28 18:34:55 +0800

    简化选择运算器

--- ap-adjustment-for-py_1.0.0/data-final-fixes.lua
+++ ap-adjustment-for-py_1.0.0/data-final-fixes.lua
@@ -39,3 +39,16 @@ data.raw.recipe["display-panel"].ingredients = {
         amount = 1
     }
 }
+
+data.raw.recipe["selector-combinator"].ingredients = {
+    {
+        type = "item",
+        name = "small-parts-01",
+        amount = 1
+    }
+}
+data.raw.technology["advanced-combinators"].unit = {
+    count = 50,
+    ingredients = {{"automation-science-pack", 1}},
+    time = 30
+}
